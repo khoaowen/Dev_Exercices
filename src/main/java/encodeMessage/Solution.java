f1d14ecc68d7f47ec0c6e1ccfcfa16208541061f
khoa <khoa@DESKTOP-531D76G>
2020-02-09 23:23:44 +0100

    First commit

---
+++ src/main/java/encodeMessage/Solution.java
@@ -0,0 +1,137 @@
+package encodeMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Auto-generated code below aims at helping you parse the standard input
+ * according to the problem statement.
+ **/
+class Solution {
+
+	public static void main(String args[]) {
+		String MESSAGE = "%";
+
+		// Write an action using System.out.println()
+		// To debug: System.err.println("Debug messages...");
+
+		System.out.println(Integer.toBinaryString('C'));
+		System.out.println(Integer.toBinaryString('%'));
+		System.out.println(convertToBit("C"));
+		System.out.println(convertToBit("%"));
+
+		System.out.println(encode(MESSAGE).equals("00 0 0 0 00 00 0 0 00 0 0 0")); // 0100101
+		// System.err.println(convertToBlocs("00000", PREFIX.ZERO).computeValue());
+		// System.err.println(convertToBlocs("0", PREFIX.ZERO).computeValue());
+		// System.err.println(convertToBlocs("11111", PREFIX.ONE).computeValue());
+		// System.err.println(convertToBlocs("1", PREFIX.ONE).computeValue());
+
+	}
+
+	public static String encode(String s) {
+		String res = convertToBit(s);
+		List<Bloc> blocs = extractBlocs(res);
+		StringBuilder bd = new StringBuilder();
+		for (int i = 0; i < blocs.size(); i++) {
+			bd.append(blocs.get(i).computeValue());
+			if (i < blocs.size() - 1) {
+				bd.append(" ");
+			}
+
+		}
+		return bd.toString();
+	}
+
+	public static List<Bloc> extractBlocs(String bits) {
+		List<Bloc> blocs = new ArrayList<Bloc>();
+		int counter = 0;
+		char firstChar = bits.charAt(0);
+		for (int i = 0; i < bits.length(); i++) {
+			if (firstChar == bits.charAt(i)) {
+				counter++;
+			} else {
+				blocs.add(new Bloc(PREFIX.fromChar(firstChar), counter));
+				firstChar = bits.charAt(i);
+				counter = 1;
+			}
+			if (i == bits.length() - 1) {
+				blocs.add(new Bloc(PREFIX.fromChar(firstChar), counter));
+			}
+		}
+		return blocs;
+	}
+
+	public static String convertToBit(String message) {
+		// stringToBinary
+		StringBuilder binary = new StringBuilder();
+		char[] charArray = message.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			int c = charArray[i];
+			binary.append(fill0For7Bits(Integer.toBinaryString(c)));
+		}
+		return binary.toString();
+	}
+
+	public static String fill0For7Bits(String bits) {
+		StringBuilder bd = new StringBuilder();
+		int missing = 7 - bits.length();
+		while (missing > 0) {
+			bd.append("0");
+			missing--;
+		}
+		return bd.append(bits).toString();
+	}
+
+	public static Bloc convertToBlocs(String bits, PREFIX prefix) {
+		return new Bloc(prefix, bits.length());
+	}
+
+	public enum PREFIX {
+		ZERO("00 "), ONE("0 ");
+
+		private final String value;
+
+		PREFIX(String val) {
+			value = val;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public static PREFIX fromChar(char c) {
+			if (c == '1') {
+				return ONE;
+			} else {
+				return ZERO;
+			}
+		}
+	}
+
+	public static class Bloc {
+		private final PREFIX prefix;
+		private final int length;
+
+		Bloc(PREFIX prefix, int length) {
+			this.prefix = prefix;
+			this.length = length;
+		}
+
+		public PREFIX getPrefix() {
+			return prefix;
+		}
+
+		public int getLength() {
+			return length;
+		}
+
+		public String computeValue() {
+			StringBuilder bd = new StringBuilder(prefix.getValue());
+			for (int i = 0; i < length; i++) {
+				bd.append("0");
+			}
+			return bd.toString();
+		}
+	}
+
+}
---
+++ src/test/java/encodeMessage/Test.java
@@ -0,0 +1,30 @@
+package encodeMessage;
+
+import org.junit.Assert;
+
+public class Test {
+
+	@org.junit.Test
+	public void testC() {
+		Assert.assertEquals("0 0 00 0000 0 00", Solution.encode("C"));
+	}
+
+	@org.junit.Test
+	public void testCC() {
+		Assert.assertEquals("0 0 00 0000 0 000 00 0000 0 00", Solution.encode("CC"));
+	}
+
+	@org.junit.Test
+	public void testSpecialCharacter() {
+		Assert.assertEquals("00 0 0 0 00 00 0 0 00 0 0 0", Solution.encode("%"));
+	}
+
+	@org.junit.Test
+	public void testChuckNorris() {
+		Assert.assertEquals(
+				"0 0 00 0000 0 0000 00 0 0 0 00 000 0 000 00 0 0 0 00 0 0 000 00 000 0 0000 00 0 0 0 00 0 0 00 00 0 0 0 00 00000 0 0 00 00 0 000 00 0 0 00 00 0 0 0000000 00 00 0 0 00 0 0 000 00 00 0 0 00 0 0 00 00 0 0 0 00 00 0 0000 00 00 0 00 00 0 0 0 00 00 0 000 00 0 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 000 00 000 0 0 00 0 0 00 00 0 0 000000 00 0000 0 0000 00 00 0 0 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 00 00 0 0 0 00 000 0 00 00 0000 0 0000 00 00 0 00 00 0 0 0 00 000000 0 00 00 00 0 0 00 00 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 0000 00 00 0 00 00 0 0 000 00 0 0 0 00 00 0 0 00 000000 0 00 00 00000 0 0 00 00000 0 00 00 0000 0 000 00 0 0 000 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 000 00 0 0 00000 00 0 0 0 00 000 0 00 00 0 0 0 00 00 0 0000 00 0 0 0 00 00 0 00 00 00 0 0 00 0 0 0 00 0 0 0 00 00000 0 000 00 00 0 00000 00 0000 0 00 00 0000 0 000 00 000 0 0000 00 00 0 0 00 0 0 0 00 0 0 0 00 0 0 000 00 0",
+				Solution.encode("Chuck Norris' keyboard has 2 keys: 0 and white space."));
+
+	}
+
+}
